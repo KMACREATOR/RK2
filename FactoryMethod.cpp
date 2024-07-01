df88e285bd4c3c9e6ef2d47111734653c0d61604
KMACREATOR <agredi2567@gmail.com>
2024-07-02 02:59:41 +0300

    major changes

--- FactoryMethod.cpp
+++ FactoryMethod.cpp
@@ -10,20 +10,41 @@
 
 #include <iostream>
 #include <string>
-
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 /*
  * Product
  * products implement the same interface so that the classes can refer
  * to the interface not the concrete product
  */
+
+using ::testing::AtLeast;
+
 class Product
 {
 public:
   virtual ~Product() {}
-  
+
   virtual std::string getName() = 0;
   // ...
 };
+
+
+//MockProduct для класса Mock
+class MockProduct : public Product {
+	MOCK_METHOD(std::string, getName(), (), (override));
+};
+
+//сразу же тестируем
+
+TEST(ProductTest, PrintingCheck){
+	MockProduct prod;
+	prod.getName();
+	EXPECT_CALL(prod, getName())
+		.Times(AtLeast(1));
+}
+
+
 
 /*
  * Concrete Product
@@ -33,13 +54,18 @@ class ConcreteProductA : public Product
 {
 public:
   ~ConcreteProductA() {}
-  
+
   std::string getName()
   {
     return "type A";
   }
   // ...
 };
+
+TEST(TestProducts, ProductA){
+	ConcreteProductA PA1;
+	EXPECT_STREQ(PA1.getName, "type A");
+}
 
 /*
  * Concrete Product
@@ -75,6 +101,14 @@ public:
   // ...
 };
 
+//MockCreator класс для Creator
+
+class MockCreator : public Creator {
+	MOCKMETHOD(Product*, createProductA (), (override));
+		
+};
+
+
 /*
  * Concrete Creator
  * implements factory method that is responsible for creating
@@ -103,6 +137,12 @@ public:
   // ...
 };
 
+//тестируем ConcreteCreator
+TEST (ConcreteTests, CreatorTets){
+	ConcreteCreator concreator1;
+	ConcreteCreator concreator2;	
+
+}
 
 int main()
 {
@@ -116,6 +156,7 @@ int main()
   std::cout << "Product: " << p2->getName() << std::endl;
   creator->removeProduct( p2 );
   
+  ::testing::InitGoogleTest(&argc, argv);
   delete creator;
-  return 0;
+  return RUN_ALL_TESTS();
 }
