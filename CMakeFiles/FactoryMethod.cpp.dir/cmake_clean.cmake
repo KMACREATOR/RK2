cdc6ce7765bc1c1911ecec1aef11c00b4773f33a
KMACREATOR <agredi2567@gmail.com>
2024-07-02 08:07:09 +0300

    minor changes

---
+++ CMakeFiles/FactoryMethod.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FactoryMethod.cpp.dir/FactoryMethod.cpp.o"
+  "FactoryMethod.cpp"
+  "FactoryMethod.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FactoryMethod.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
